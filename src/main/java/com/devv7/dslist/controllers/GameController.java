b86dc198bb0c3008e7577ad5fd5320d48ee8ce83
Vinícius Souza <viniciusouza7@gmail.com>
2023-07-12 11:33:33 -0300

    GET game by id

--- src/main/java/com/devv7/dslist/controllers/GameController.java
+++ src/main/java/com/devv7/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.devv7.dslist.controllers;
 
+import com.devv7.dslist.dtos.GameDTO;
 import com.devv7.dslist.dtos.GameMinDTO;
 import com.devv7.dslist.entities.Game;
 import com.devv7.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,11 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll() {
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
     }
 
 }
--- src/main/java/com/devv7/dslist/services/GameService.java
+++ src/main/java/com/devv7/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.devv7.dslist.services;
 
+import com.devv7.dslist.dtos.GameDTO;
 import com.devv7.dslist.dtos.GameMinDTO;
 import com.devv7.dslist.entities.Game;
 import com.devv7.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,8 +16,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         return gameRepository.findAll().stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        return new GameDTO(gameRepository.findById(id).get());
     }
 }
 
